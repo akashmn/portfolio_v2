d3909e1e79efb8e9be38a40699cff2c3f29cb5ee
Akash M Nandan <akash003nandan@gmail.com>
2025-08-20 20:55:12 +0530

    chores: project clean up - boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-function App() {
+import React from 'react'
+
+const App = () => {
     return (
-        <div className="min-h-screen bg-black-200 p-4">
-            <div className="sm:px-10 px-5">
-                <h1 className="text-3xl font-semibold text-white bg-red-500 p-4 rounded">
-                    🚀 Tailwind v3 is working!
-                </h1>
-            </div>
-        </div>
+        <div className="bg-red-500 text-white underline">App</div>
     )
 }
-
 export default App
