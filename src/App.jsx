2f38cb6034fd25a58c19541ddd0ad996fedd1615
Akash M Nandan <akash003nandan@gmail.com>
2025-08-20 06:15:17 +0530

    chores: tailwindcss@^3.4.0 setup and tailwind.config.js updated with code snippets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
-  return (
-      <>
-        <h1>Welcome to My App</h1>
-      </>
-  )
+    return (
+        <div className="min-h-screen bg-black-200 p-4">
+            <div className="sm:px-10 px-5">
+                <h1 className="text-3xl font-semibold text-white bg-red-500 p-4 rounded">
+                    🚀 Tailwind v3 is working!
+                </h1>
+            </div>
+        </div>
+    )
 }
 
 export default App
