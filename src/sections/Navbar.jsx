a1e141bded1f2a056a2f7f081fdc17fd3e6c06f1
Akash M Nandan <akash003nandan@gmail.com>
2025-08-28 12:24:41 +0530

    feat(ui): conditional rendering onclick on menu toggle of navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+      setIsOpen((prevIsOpen) => !prevIsOpen);
+    }
+
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
                 <div className="flex mx-auto justify-between items-center py-5 c-space">
+                    {/*instagram handle @akashmnandan*/}
                     <a
                     href="https://www.instagram.com/akashmnandan"
                     target="_blank"
@@ -13,6 +20,11 @@ const Navbar = () => {
                     >
                         @akashmnandan
                     </a>
+
+                    {/*menu icon*/}
+                    <button onClick={toggleMenu}>
+                        <img src={!isOpen? "/assets/menu.svg": "/assets/close.svg"} alt="menu"  className="w-6 h-6"/>
+                    </button>
                 </div>
             </div>
         </header>
