49beabaa2855bee2165bd5c107012b691fa897c5
Akash M Nandan <akash003nandan@gmail.com>
2025-08-29 01:13:06 +0530

    commenting Leva controls

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,15 +10,15 @@ import {calculateSizes} from "../constants/index.js";
 const Hero = () => {
 
     // Leva controls for debugging and adjusting the 3D model's position, rotation, and scale
-    const x = useControls('HackerRoom', {
-        positionX: {value: 0, min: -10, max: 10, step: 0.01},
-        positionY: {value: 0, min: -10, max: 10, step: 0.01},
-        positionZ: {value: 0, min: -10, max: 10, step: 0.01},
-        rotationX: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
-        rotationY: {value: Math.PI * 2, min: -Math.PI, max: Math.PI, step: 0.01},
-        rotationZ: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
-        scale: {value: 0, min: 0, max: 100, step: 0.01},
-    })
+    // const x = useControls('HackerRoom', {
+    //     positionX: {value: 0, min: -10, max: 10, step: 0.01},
+    //     positionY: {value: 0, min: -10, max: 10, step: 0.01},
+    //     positionZ: {value: 0, min: -10, max: 10, step: 0.01},
+    //     rotationX: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
+    //     rotationY: {value: Math.PI * 2, min: -Math.PI, max: Math.PI, step: 0.01},
+    //     rotationZ: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
+    //     scale: {value: 0, min: 0, max: 100, step: 0.01},
+    // })
 
     //media query for responsiveness
     const isSmallMobile = useMediaQuery({query: '(max-width: 480px)'})
@@ -41,7 +41,7 @@ const Hero = () => {
 
             {/*hero section 3js models*/}
             <div className='w-full h-full absolute inset-8'>
-                <Leva />
+                {/*<Leva />*/}
                 <Canvas className='h-full w-full'>
                     <Suspense fallback={<CanvasLoader />}>
                         <PerspectiveCamera makeDefault position={[0, 0, 15]} />
