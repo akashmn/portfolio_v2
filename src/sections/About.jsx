1308ccb221ffc4d6b6aaf0e769a0908b5420cf6a
Akash M Nandan <akash003nandan@gmail.com>
2025-08-29 17:07:43 +0530

    feat(ui): About me grid 2 added - tech Stacks

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,6 +6,8 @@ const About = () => {
     return (
         <section className="c-space my-20" id="about">
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+
+                {/*Hi, im akash M Nandan*/}
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
@@ -18,6 +20,22 @@ const About = () => {
                         </div>
                     </div>
                 </div>
+
+                {/*Tech stacks*/}
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">Tech Stack</p>
+                            <p className="grid-subtext">
+                                I specialize in frontend development, particularly using React.js, JavaScript, HTML/CSS, and Tailwind CSS.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </section>
     );
