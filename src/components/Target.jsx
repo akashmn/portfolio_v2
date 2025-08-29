8c02900630e2632a38291a4667ab307692bfc477
Akash M Nandan <akash003nandan@gmail.com>
2025-08-29 12:32:37 +0530

    style(target Component): gsap animation added

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,9 +1,21 @@
 import React, {useRef} from 'react'
 import {useGLTF} from "@react-three/drei";
+import {useGSAP} from "@gsap/react";
+import {gsap} from "gsap";
 
 const Target = (props) => {
     const targetRef = useRef()
     const {scene} = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+
+    useGSAP(() => {
+        gsap.to(targetRef.current.rotation, {
+            y: "+=6.28319", // 2 * Math.PI radians = 360 degrees
+            duration: 20,
+            ease: "none",
+            repeat: -1,
+            yoyo: true,
+        })
+    })
 
     return (
         <mesh {...props} ref={targetRef}>
