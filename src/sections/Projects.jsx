92a5c6577f59a3585930224d380bac6207c1f76c
Akash M Nandan <akash003nandan@gmail.com>
2025-08-30 12:52:25 +0530

    feat(ui): threejs model added in projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,13 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Center, OrbitControls } from '@react-three/drei';
 
 import { myProjects } from '../constants/index.js';
+import CanvasLoader from '../components/CanvasLoader.jsx';
+import DemoComputer from '../components/DemoComputer.jsx';
+
 const projectCount = myProjects.length;
 
 const Projects = () => {
@@ -73,6 +78,21 @@ const Projects = () => {
                             <img src="/assets/right-arrow.png" alt="right arrow" className="w-4 h-4" />
                         </button>
                     </div>
+                </div>
+
+                <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+                    <Canvas>
+                        <ambientLight intensity={Math.PI} />
+                        <directionalLight position={[10, 10, 5]} />
+                        <Center>
+                            <Suspense fallback={<CanvasLoader />}>
+                                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                    <DemoComputer texture={currentProject.texture} />
+                                </group>
+                            </Suspense>
+                        </Center>
+                        <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+                    </Canvas>
                 </div>
             </div>
         </section>
